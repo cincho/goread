05ac775b7bd4376b251e2f48220372f2b59bd55a
cincho <92636566+cincho@users.noreply.github.com>
2022-07-07 14:38:11 +0100

    Console command interface added.

---
+++ src/Command/Command.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cincho\Reader\Command;
+
+interface Command
+{
+    public const EXIT_SUCCESS = 0;
+    public const EXIT_FAILURE = 1;
+
+    public function execute(): int;
+}
