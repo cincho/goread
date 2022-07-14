482263272ab815c6041c718bdba5fda742aaa36a
cincho <92636566+cincho@users.noreply.github.com>
2022-07-14 15:04:51 +0100

    Composer scripts added and test corrected.

--- tests/Reader/Unit/Controller/IndexControllerTest.php
+++ tests/Reader/Unit/Controller/IndexControllerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Cincho\Reader\Test\Unit\DependencyInjection;
 
 use Cincho\Framework\Http\Response;
+use Cincho\Framework\Template\Engine;
 use Cincho\Reader\Controller\IndexController;
 use PHPUnit\Framework\TestCase;
 
@@ -12,12 +13,14 @@ class IndexControllerTest extends TestCase
 {
     public function testInstanceOf(): void
     {
-        $this->assertInstanceOf(IndexController::class, new IndexController());
+        $engine = $this->createMock(Engine::class);
+        $this->assertInstanceOf(IndexController::class, new IndexController($engine));
     }
 
     public function testInvoke(): void
     {
-        $controller = new IndexController();
+        $engine = $this->createMock(Engine::class);
+        $controller = new IndexController($engine);
 
         $this->assertInstanceOf(Response::class, $controller());
     }
