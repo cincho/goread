6587cc84a03ee842e707ed5efaac0c581973d172
cincho <92636566+cincho@users.noreply.github.com>
2022-07-16 13:56:37 +0100

    Base template removed.

--- src/Template/index.php
+++ src/Template/index.php
@@ -1,14 +1,15 @@
-<?php $this->extend('base.php'); ?>
-
-<?php $this->start('content'); ?>
-
-    <div id="app"></div>
-
-<?php $this->end('content'); ?>
-
-<?php $this->start('scripts'); ?>
-
-    <script src="/js/app.js"></script>
-
-<?php $this->end('scripts'); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Reader</title>
+        <link rel="icon" href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/4QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAERERERERERERAAAAAAAAERAQAAAAAAEBEAEAAAAAEAEQABAAAAEAARAAAQAAEAABEAAAEAEAAAEQAAABEAAAARAAAAEQAAABEAAAEAEAAAEQAAEAABAAARAAEAAAAQABEAEAAAAAEAEQEAAAAAABAREAAAAAAAAREREREREREREAAAAAP/wAAF/6AABv9gAAd+4AAHveAAB9vgAAfn4AAH5+AAB9vgAAe94AAHfuAABv9gAAX/oAAD/8AAAAAAAA" type="image/x-icon" />
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app"></script>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
 
